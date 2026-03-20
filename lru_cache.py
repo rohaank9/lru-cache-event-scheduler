e893445167ad7952f97282563d4f09f239992390
rohaank9 <59990275+rohaank9@users.noreply.github.com>
2026-03-20 22:34:28 +0530

    Update lru_cache.py

--- lru_cache.py
+++ lru_cache.py
@@ -1,19 +1,58 @@
-from collections import OrderedDict
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = OrderedDict()
+        self.cache = {}  # key -> node
+
+        # Dummy head & tail (to avoid edge cases)
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # Remove node from DLL
+    def _remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    # Insert node right after head (most recently used)
+    def _insert(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._insert(node)  # move to front
+            return node.value
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
+            self._remove(self.cache[key])
+
+        node = Node(key, value)
+        self._insert(node)
+        self.cache[key] = node
+
         if len(self.cache) > self.capacity:
-            self.cache.popitem(last=False)
+            # remove LRU (node before tail)
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
+lru = LRUCache(2)
+
